ec75b9c5bc5bc2205008239600e638e624b90b28
Ben Winchester <benedict.winchester@gmail.com>
2024-02-21 16:08:33 +0000

    :tada: Add pv-cell file

---
+++ src/polytunnelpv/pv_module/pv_cell.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.10
+########################################################################################
+# pv_cell.py - Curved-PV-module cell Python module.                                    #
+#                                                                                      #
+# Author: Yaar Safra, Ben Winchester                                                   #
+# Copyright: Yaar Safra, 2024                                                          #
+# Date created: 21/02/2024                                                             #
+# License: Open source                                                                 #
+########################################################################################
+"""
+pv_cell.py - The pv-cell module for Polytunnel-PV.
+
+This module provides functionality for the modelling of the individual cells within the
+curved PV module.
+
+"""
