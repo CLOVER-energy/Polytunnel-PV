cb033298c9db1a3e0aaacc5ce014306b156f6317
Ben Winchester <benedict.winchester@gmail.com>
2024-02-21 16:00:38 +0000

    Add main module functionality

---
+++ src/polytunnelpv/__main__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3.10
+########################################################################################
+# __main__.py - Main module for Polytunnel-PV.                                         #
+#                                                                                      #
+# Author: Ben Winchester                                                               #
+# Copyright: Ben Winchester, 2024                                                      #
+# Date created: 21/02/2024                                                             #
+# License: Open source                                                                 #
+########################################################################################
+"""
+__main__.py - The main module for Polytunnel-PV.
+
+Polytunnel-PV simulates the performance of curved photovoltaic modules for polytunnel
+and greenhouse applications. This main module provides a command-line interface
+entrypoint for executing the model.
+
+"""
+
+__version__ = "0.0.1"
+
+import sys
+
+def main(unparsed_arguments) -> None:
+    """
+    Main method for Polytunnel-PV.
+
+    """
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
