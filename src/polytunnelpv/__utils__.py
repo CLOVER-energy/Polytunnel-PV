341e87260aa04c932440754a98a7f664fa84ff9a
Ben Winchester <benedict.winchester@gmail.com>
2024-02-22 16:24:33 +0000

    :tada: Create blank utility module

---
+++ src/polytunnelpv/__utils__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.10
+########################################################################################
+# __utils__.py - Utility module for Polytunnel-PV.                                     #
+#                                                                                      #
+# Author: Ben Winchester                                                               #
+# Copyright: Ben Winchester, 2024                                                      #
+# Date created: 22/02/2024                                                             #
+# License: Open source                                                                 #
+########################################################################################
+"""
+__utils__.py - The utility module for Polytunnel-PV.
+
+"""
