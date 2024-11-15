69514a76e1561ba144374493c401d732a775a910
BenWinchester <benedict.winchester@gmail.com>
2024-11-15 18:01:56 +0000

    :tada: Create validation script

--- bin/validation.sh
+++ bin/validation.sh
@@ -1,2 +1,16 @@
+########################################################################################
+# validation.sh - Script for executing Polytunnel-PV validation a job on the HPC.      #
+#                                                                                      #
+# Author: Ben Winchester                                                               #
+# Copyright: Ben Winchester, 2024                                                      #
+# Date created: 15/11/2024                                                             #
+#                                                                                      #
+# For more information, please email:                                                  #
+#   benedict.winchester@gmail.com                                                      #
+########################################################################################
+#PBS -l walltime=72:00:00
+#PBS -l select=1:ncpus=1:mem=8Gb
+#PBS -N polytunnel-pv-validation
 
-python -m src.polytunnelpv --scenario circular_polysolar_kent_bypassed --operating-mode validation --timestamps-file hourly_december_data_martyn.csv
+python -m src.polytunnelpv --scenario circular_polysolar_kent_bypassed \
+    --operating-mode validation --timestamps-file hourly_december_data_martyn.csv
