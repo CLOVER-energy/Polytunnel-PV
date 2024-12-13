b221ef9b62d23d3816047e8a4675060dba538f67
BenWinchester <benedict.winchester@gmail.com>
2024-12-13 10:49:51 +0000

    Add functionality for July 1st and 2nd

--- output_combination_script.py
+++ output_combination_script.py
@@ -30,7 +30,8 @@ def main() -> None:
 
     # Go through each file in the outputs directory and extract the total output data
     # at each time.
-    filename_to_data_map: dict[str, list[float]] = {}
+    filename_to_july_1st_data_map: dict[str, list[float]] = {}
+    filename_to_july_2nd_data_map: dict[str, list[float]] = {}
     for filename in tqdm(
         os.listdir(output_dirname), desc="Parsing files", leave=True, unit="file"
     ):
@@ -39,18 +40,31 @@ def main() -> None:
         ) as output_file:
             filedata = json.load(output_file)
 
-        filename_to_data_map[filename.split("hourly_mpp_")[1].split("_4344_to")[0]] = [
-            entry[2] for entry in filedata
-        ]
+        if "4344" in filename:
+            filename_to_july_1st_data_map[
+                filename.split("hourly_mpp_")[1].split("_4344_to")[0]
+            ] = [entry[2] for entry in filedata]
+
+        if "4368" in filename:
+            filename_to_july_2nd_data_map[
+                filename.split("hourly_mpp_")[1].split("_4368_to")[0]
+            ] = [entry[2] for entry in filedata]
 
     time_series = [entry[1] for entry in filedata]
-    filename_to_data_map["hour"] = time_series
+    filename_to_july_1st_data_map["hour"] = time_series
+    filename_to_july_2nd_data_map["hour"] = time_series
 
-    combined_frame = pd.DataFrame(filename_to_data_map)
+    combined_july_1st_frame = pd.DataFrame(filename_to_july_1st_data_map)
     with open(
-        "combined_bypass_diode_investigation.csv", "w", encoding="UTF-8"
+        "combined_bypass_diode_investigation_july_1st.csv", "w", encoding="UTF-8"
     ) as combined_file:
-        combined_frame.to_csv(combined_file)
+        combined_july_1st_frame.to_csv(combined_file)
+
+    combined_july_2nd_frame = pd.DataFrame(filename_to_july_2nd_data_map)
+    with open(
+        "combined_bypass_diode_investigation_july_1st.csv", "w", encoding="UTF-8"
+    ) as combined_file:
+        combined_july_2nd_frame.to_csv(combined_file)
 
 
 if __name__ == "__main__":
