:bug: Fix bug in the MPP calculation with indexing

post_hpc_analysis.py
@@ -136,19 +136,43 @@ def main(args: list[Any]) -> None:
 
     # Remove ludicrous values
     combined_data.clip(lower=0, upper=5000, inplace=True)
-    combined_data = combined_data[[entry for entry in combined_data if combined_data[entry].max(axis=0) < 5000]]
+    combined_data = combined_data[
+        [entry for entry in combined_data if combined_data[entry].max(axis=0) < 5000]
+    ]
 
     # Determine scenario information relevant for analysis.
-    relevant_scenarios = {entry.name: entry for entry in scenarios if entry.name in combined_data.columns}
-    bypass_diodes = {scenario.name: [entry for entry in scenario.pv_module.pv_cells_and_cell_strings if not isinstance(entry, PVCell)] for scenario in relevant_scenarios.values()}
-    num_bypass_diodes = {scenario_name: len(diodes) for scenario_name, diodes in bypass_diodes.items()}
+    relevant_scenarios = {
+        entry.name: entry for entry in scenarios if entry.name in combined_data.columns
+    }
+    bypass_diodes = {
+        scenario.name: [
+            entry
+            for entry in scenario.pv_module.pv_cells_and_cell_strings
+            if not isinstance(entry, PVCell)
+        ]
+        for scenario in relevant_scenarios.values()
+    }
+    num_bypass_diodes = {
+        scenario_name: len(diodes) for scenario_name, diodes in bypass_diodes.items()
+    }
 
-    num_diodes_frame = pd.DataFrame({"scenario": num_bypass_diodes.keys(), "num_diodes": num_bypass_diodes.values()})
+    num_diodes_frame = pd.DataFrame(
+        {"scenario": num_bypass_diodes.keys(), "num_diodes": num_bypass_diodes.values()}
+    )
     num_diodes_frame.index = num_diodes_frame["scenario"]
     num_diodes_frame.pop("scenario")
 
     # Determine the impact of the number of bypass diodes, regardless of length
-    diodes_to_frame: dict[int, pd.DataFrame] = {num_diodes: combined_data[[entry for entry in num_diodes_frame.index if num_diodes_frame.loc[entry, "num_diodes"] == num_diodes]] for num_diodes in set(num_bypass_diodes.values())}
+    diodes_to_frame: dict[int, pd.DataFrame] = {
+        num_diodes: combined_data[
+            [
+                entry
+                for entry in num_diodes_frame.index
+                if num_diodes_frame.loc[entry, "num_diodes"] == num_diodes
+            ]
+        ]
+        for num_diodes in set(num_bypass_diodes.values())
+    }
     sns.set_palette(
         sns.color_palette(
             [
@@ -168,14 +192,19 @@ def main(args: list[Any]) -> None:
         )
     )
 
-    sns.set_palette(sns.cubehelix_palette(start=0.6, rot=-0.6, n_colors=len(set(diodes_to_frame.keys()))))
+    sns.set_palette(
+        sns.cubehelix_palette(
+            start=0.6, rot=-0.6, n_colors=len(set(diodes_to_frame.keys()))
+        )
+    )
 
-    plt.figure(figsize=(48/5, 32/5))
+    plt.figure(figsize=(48 / 5, 32 / 5))
     for num_diodes, sub_frame in reversed(diodes_to_frame.items()):
         sns.scatterplot(
             sub_frame.mean(axis=1),
-            alpha=0.85,
+            alpha=0.55,
             label=num_diodes,
+            linewidth=0,
             marker="h",
             s=250,
         )
@@ -202,20 +231,40 @@ def main(args: list[Any]) -> None:
 
     plt.xlabel("Hour of the day")
     plt.ylabel("System-wide power produced / kW")
-    plt.savefig(f"hourly_power_scatter_{os.path.basename(combined_output_filename).replace('.csv', '')}.pdf", format="pdf", bbox_inches="tight", pad_inches=0)
+    plt.savefig(
+        f"hourly_power_scatter_{os.path.basename(combined_output_filename).replace('.csv', '')}.pdf",
+        format="pdf",
+        bbox_inches="tight",
+        pad_inches=0,
+    )
 
     plt.show()
 
-    diodes_to_power = {num_diodes: pd.DataFrame({"power": frame.sum(axis=0).transpose().reset_index(drop=True)}) for num_diodes, frame in diodes_to_frame.items()}
+    diodes_to_power = {
+        num_diodes: pd.DataFrame(
+            {"power": frame.sum(axis=0).transpose().reset_index(drop=True)}
+        )
+        for num_diodes, frame in diodes_to_frame.items()
+    }
     for num_diodes, frame in diodes_to_power.items():
         frame["num_diodes"] = num_diodes
 
     num_diodes_power_frame = pd.concat(diodes_to_power.values(), axis=0)
 
     # Bypass didoe number impact
-    cat_plot = sns.catplot(num_diodes_power_frame, kind="boxen", x="num_diodes", y="power", height=6.5, aspect=(48/32), palette=sns.color_palette(), 
-    linewidth=0, linecolor=".7",
-    line_kws=dict(linewidth=0, color="#cde"), showfliers=True)
+    cat_plot = sns.catplot(
+        num_diodes_power_frame,
+        kind="boxen",
+        x="num_diodes",
+        y="power",
+        height=6.5,
+        aspect=(48 / 32),
+        palette=sns.color_palette(),
+        linewidth=0,
+        linecolor=".7",
+        line_kws=dict(linewidth=0, color="#cde"),
+        showfliers=True,
+    )
 
     plt.xlabel("Number of bypass diodes installed")
     plt.ylabel("System-wide power produced / kW")
@@ -240,33 +289,62 @@ def main(args: list[Any]) -> None:
     plt.legend().remove()
     plt.ylim(0, None)
 
-    medians = num_diodes_power_frame.groupby(['num_diodes'])['power'].median()
-    vertical_offset = num_diodes_power_frame['power'].median() * 0.05 # offset from median for display
+    medians = num_diodes_power_frame.groupby(["num_diodes"])["power"].median()
+    vertical_offset = (
+        num_diodes_power_frame["power"].median() * 0.05
+    )  # offset from median for display
 
-    for xtick, xtick_position in zip(cat_plot.ax.get_xticklabels(), cat_plot.ax.get_xticks()):
-        if (xtick_value:=int(xtick._text)) < plt.xlim()[0]:
+    for xtick, xtick_position in zip(
+        cat_plot.ax.get_xticklabels(), cat_plot.ax.get_xticks()
+    ):
+        if (xtick_value := int(xtick._text)) < plt.xlim()[0]:
             continue
-        cat_plot.ax.text(xtick_position,medians[xtick_value] + vertical_offset,f"{medians[xtick_value]:.0f}",
-            horizontalalignment='center',size='x-small',color=sns.color_palette().as_hex()[-1],weight='semibold')
+        cat_plot.ax.text(
+            xtick_position,
+            medians[xtick_value] + vertical_offset,
+            f"{medians[xtick_value]:.0f}",
+            horizontalalignment="center",
+            size="x-small",
+            color=sns.color_palette().as_hex()[-1],
+            weight="semibold",
+        )
 
     for ax in plt.gcf().axes:
         median = num_diodes_power_frame.groupby("num_diodes")["power"].median()
         for index, med in enumerate(median.values):
-            ax.axhline(xmin=(x_pos:=index / len(median.values)), y=med, xmax=x_pos+1 /len(median.values), color='white')
+            ax.axhline(
+                xmin=(x_pos := index / len(median.values)),
+                y=med,
+                xmax=x_pos + 1 / len(median.values),
+                color="white",
+            )
 
-    plt.savefig(f"number_of_diodes_{os.path.basename(combined_output_filename).replace('.csv', '')}.pdf", format="pdf", bbox_inches="tight", pad_inches=0)
+    plt.savefig(
+        f"number_of_diodes_{os.path.basename(combined_output_filename).replace('.csv', '')}.pdf",
+        format="pdf",
+        bbox_inches="tight",
+        pad_inches=0,
+    )
 
     plt.show()
 
     # Plot staggered box plots
-    plt.figure(figsize=(48/5, 32/5))
+    plt.figure(figsize=(48 / 5, 32 / 5))
     melted_frames = []
     for num_diodes, sub_frame in reversed(diodes_to_frame.items()):
-        melted_frame = sub_frame.reset_index().melt(id_vars="index", var_name="scenario")
+        melted_frame = sub_frame.reset_index().melt(
+            id_vars="index", var_name="scenario"
+        )
         melted_frame["bypass_diodes"] = num_diodes
         melted_frames.append(melted_frame)
 
-    sns.boxplot(combined_melted_frame:=pd.concat(melted_frames, axis=0), x="index", y="value", hue="bypass_diodes", dodge=True)
+    sns.boxplot(
+        combined_melted_frame := pd.concat(melted_frames, axis=0),
+        x="index",
+        y="value",
+        hue="bypass_diodes",
+        dodge=True,
+    )
 
     plt.xlim(5, 19)
 
@@ -291,13 +369,22 @@ def main(args: list[Any]) -> None:
     plt.legend().remove()
     plt.show()
 
-    joint_plot = sns.jointplot(combined_melted_frame, x="index", y="value", hue="bypass_diodes", palette=sns.color_palette(),
-            marginal_ticks=True,
-            height=6.5,
-            ratio=6)
+    joint_plot = sns.jointplot(
+        combined_melted_frame,
+        x="index",
+        y="value",
+        hue="bypass_diodes",
+        palette=sns.color_palette(),
+        marginal_ticks=True,
+        height=6.5,
+        ratio=6,
+    )
 
     sns.scatterplot(
-        combined_melted_frame, x="index", y="value", hue="bypass_diodes",
+        combined_melted_frame,
+        x="index",
+        y="value",
+        hue="bypass_diodes",
         alpha=0.85,
         ax=joint_plot.ax_joint,
         # cbar=True,
@@ -341,24 +428,224 @@ def main(args: list[Any]) -> None:
         # left=0.1,
         right=1.1
     )
-    plt.savefig(f"bypass_diode_joint_plot_{os.path.basename(combined_output_filename).replace('.csv', '')}.pdf", format="pdf", bbox_inches="tight", pad_inches=0)
+    plt.savefig(
+        f"bypass_diode_joint_plot_{os.path.basename(combined_output_filename).replace('.csv', '')}.pdf",
+        format="pdf",
+        bbox_inches="tight",
+        pad_inches=0,
+    )
     plt.show()
 
-    sorted_combined_data = combined_data[combined_data.sum(axis=0).sort_values(ascending=False).index]
+    sorted_combined_data = combined_data[
+        combined_data.sum(axis=0).sort_values(ascending=False).index
+    ]
     sorted_combined_data.pop("hour")
-    sorted_combined_data.columns = [f"Scenario {entry.split('n_')[1].split('_4344')[0].split('_4368')[0]}" for entry in sorted_combined_data.columns]
+    sorted_combined_data.columns = [
+        f"Scenario {entry.split('n_')[1].split('_4344')[0].split('_4368')[0]}"
+        for entry in sorted_combined_data.columns
+    ]
 
-    sns.lineplot(sorted_combined_data[reversed(sorted_combined_data.columns[:len(sns.color_palette())])])
+    # Plot the top performing scenarios
+    plt.figure(figsize=(48 / 5, 32 / 5))
+    sns.lineplot(
+        sliced_data := sorted_combined_data[
+            reversed(sorted_combined_data.columns[: len(sns.color_palette())])
+        ]
+    )
+
+    for index, column in enumerate(sliced_data.columns):
+        plt.fill_between(
+            sliced_data.index,
+            sorted_combined_data[sorted_combined_data.columns[-1]],
+            sliced_data[column],
+            color=f"C{index}",
+            alpha=0.1,
+        )
+
+    plt.xlabel("Hour of the day")
+    plt.ylabel("System-wide power produced / kW")
+
+    plt.xlim(4, 20)
+    plt.ylim(0, None)
+
+    plt.legend(ncols=2, title="Scenario ID")
+    handles, labels = plt.gca().get_legend_handles_labels()
+    plt.savefig(
+        f"top_scenarios_{os.path.basename(combined_output_filename).replace('.csv', '')}.pdf",
+        format="pdf",
+        bbox_inches="tight",
+        pad_inches=0,
+    )
+
+    plt.show()
+
+    plt.figure(figsize=(48 / 5, 32 / 5))
+
+    for index, column in enumerate(sliced_data.columns):
+        plt.fill_between(
+            sliced_data.index,
+            sorted_combined_data[sorted_combined_data.columns[-1]],
+            sliced_data[column],
+            color=f"C{index}",
+            alpha=0.1,
+        )
+
+    plt.xlabel("Hour of the day")
+    plt.ylabel("System-wide power produced / kW")
+
+    plt.xlim(4, 20)
+    plt.ylim(0, None)
+
+    plt.legend(handles, labels, ncols=2, title="Scenario ID")
+
+    plt.savefig(
+        f"top_scenarios_fill_only_{os.path.basename(combined_output_filename).replace('.csv', '')}.pdf",
+        format="pdf",
+        bbox_inches="tight",
+        pad_inches=0,
+    )
+    plt.show()
+
+    # Plot the next set of scenarios
+    plt.figure(figsize=(48 / 5, 32 / 5))
+    sns.lineplot(
+        sliced_data := sorted_combined_data[
+            reversed(
+                sorted_combined_data.columns[
+                    len(sns.color_palette()) : 2 * len(sns.color_palette())
+                ]
+            )
+        ]
+    )
+
+    for index, column in enumerate(sliced_data.columns):
+        plt.fill_between(
+            sliced_data.index,
+            sorted_combined_data[sorted_combined_data.columns[-1]],
+            sliced_data[column],
+            color=f"C{index}",
+            alpha=0.1,
+        )
+
+    plt.xlabel("Hour of the day")
+    plt.ylabel("System-wide power produced / kW")
+
+    plt.xlim(4, 20)
+    plt.ylim(0, None)
+
+    plt.legend(ncols=2, title="Scenario ID")
+    handles, labels = plt.gca().get_legend_handles_labels()
+
+    plt.savefig(
+        f"second_scenarios_{os.path.basename(combined_output_filename).replace('.csv', '')}.pdf",
+        format="pdf",
+        bbox_inches="tight",
+        pad_inches=0,
+    )
+    plt.show()
+
+    plt.figure(figsize=(48 / 5, 32 / 5))
+    for index, column in enumerate(sliced_data.columns):
+        plt.fill_between(
+            sliced_data.index,
+            sorted_combined_data[sorted_combined_data.columns[-1]],
+            sliced_data[column],
+            color=f"C{index}",
+            alpha=0.1,
+        )
+
+    plt.xlabel("Hour of the day")
+    plt.ylabel("System-wide power produced / kW")
+
+    plt.xlim(4, 20)
+    plt.ylim(0, None)
+
+    plt.legend(handles, labels, ncols=2, title="Scenario ID")
+
+    plt.savefig(
+        f"second_scenarios_fill_only_{os.path.basename(combined_output_filename).replace('.csv', '')}.pdf",
+        format="pdf",
+        bbox_inches="tight",
+        pad_inches=0,
+    )
+    plt.show()
+
+    # Plot the next set of scenarios
+    plt.figure(figsize=(48 / 5, 32 / 5))
+    sns.lineplot(
+        sliced_data := sorted_combined_data[
+            reversed(
+                sorted_combined_data.columns[
+                    2 * len(sns.color_palette()) : 3 * len(sns.color_palette())
+                ]
+            )
+        ]
+    )
+
+    for index, column in enumerate(sliced_data.columns):
+        plt.fill_between(
+            sliced_data.index,
+            sorted_combined_data[sorted_combined_data.columns[-1]],
+            sliced_data[column],
+            color=f"C{index}",
+            alpha=0.1,
+        )
+
+    plt.xlabel("Hour of the day")
+    plt.ylabel("System-wide power produced / kW")
+
+    plt.xlim(4, 20)
+    plt.ylim(0, None)
+
+    plt.legend(ncols=2, title="Scenario ID")
+    handles, labels = plt.gca().get_legend_handles_labels()
+
+    plt.savefig(
+        f"third_scenarios_{os.path.basename(combined_output_filename).replace('.csv', '')}.pdf",
+        format="pdf",
+        bbox_inches="tight",
+        pad_inches=0,
+    )
+    plt.show()
+
+    plt.figure(figsize=(48 / 5, 32 / 5))
+
+    for index, column in enumerate(sliced_data.columns):
+        plt.fill_between(
+            sliced_data.index,
+            sorted_combined_data[sorted_combined_data.columns[-1]],
+            sliced_data[column],
+            color=f"C{index}",
+            alpha=0.1,
+        )
+
+    plt.xlabel("Hour of the day")
+    plt.ylabel("System-wide power produced / kW")
+
+    plt.xlim(4, 20)
+    plt.ylim(0, None)
+
+    plt.legend(handles, labels, ncols=2, title="Scenario ID")
+
+    plt.savefig(
+        f"third_scenarios_fill_only_{os.path.basename(combined_output_filename).replace('.csv', '')}.pdf",
+        format="pdf",
+        bbox_inches="tight",
+        pad_inches=0,
+    )
     plt.show()
 
     import pdb
 
     pdb.set_trace()
 
-    for num_diodes, sub_frame in combined_frame.groupby(combined_frame.loc["num_diodes"]) :
+    for num_diodes, sub_frame in combined_frame.groupby(
+        combined_frame.loc["num_diodes"]
+    ):
         sns.catplot(sub_frame, kind="box")
 
     plt.show()
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])

src/polytunnelpv/__main__.py
@@ -81,6 +81,7 @@ sns.set_context("notebook")
 sns.set_style("ticks")
 
 import warnings
+
 warnings.filterwarnings("ignore")
 
 # BYPASS_DIODES:
@@ -955,11 +956,19 @@ def process_single_mpp_calculation_without_pbar(
             for pv_cell in scenario.pv_module.pv_cells_and_cell_strings
         }
 
+        sampling_current_series = np.array(
+            [
+                entry
+                for entry in sorted(current_series[::CURRENT_SAMPLING_RATE])
+                if entry <= 10
+            ]
+        )
+
         cellwise_voltage = {
             pv_cell: [
                 interpreter(current)
                 for current in tqdm(
-                    current_series[::CURRENT_SAMPLING_RATE],
+                    sampling_current_series,
                     desc="Interpolation calculation",
                     leave=False,
                 )
@@ -972,7 +981,7 @@ def process_single_mpp_calculation_without_pbar(
         }
         module_voltage = [sum(sublist) for sublist in zip(*cellwise_voltage.values())]
 
-        module_power = module_voltage * current_series[::CURRENT_SAMPLING_RATE]
+        module_power = module_voltage * sampling_current_series
         mpp_index: int = list(module_power).index(np.max(module_power))
 
         # Determine the power through the module at MPP
@@ -981,7 +990,7 @@ def process_single_mpp_calculation_without_pbar(
         # Determine the MPP power produced by each cell
         cellwise_mpp: dict[PVCell, float] = {
             pv_cell: voltage_interpreter(
-                (mpp_current := current_series[::CURRENT_SAMPLING_RATE][mpp_index])
+                (mpp_current := sampling_current_series[mpp_index])
             )
             * mpp_current
             for pv_cell, voltage_interpreter in cell_voltage_interpreters.items()
@@ -1665,13 +1674,15 @@ def main(unparsed_arguments) -> None:
             # Parallelised MPP computation #
             ################################
 
-            # _, mpp_power, bypassed_cell_strings = process_single_mpp_calculation_without_pbar(
-            #     8,
-            #     irradiance_frame=irradiance_frame,
-            #     locations_to_weather_and_solar_map=locations_to_weather_and_solar_map,
-            #     pv_system=pv_system,
-            #     scenario=modelling_scenario,
-            # )
+            _, mpp_power, bypassed_cell_strings = (
+                process_single_mpp_calculation_without_pbar(
+                    parsed_args.start_day_index,
+                    irradiance_frame=irradiance_frame,
+                    locations_to_weather_and_solar_map=locations_to_weather_and_solar_map,
+                    pv_system=pv_system,
+                    scenario=modelling_scenario,
+                )
+            )
 
             # Use joblib to parallelize the for loop
             start_time = time.time()
@@ -2220,7 +2231,7 @@ def main(unparsed_arguments) -> None:
                 * max(
                     [
                         pv_cell.short_circuit_current
-                        for pv_cell in scenario.pv_module.pv_cells
+                        for pv_cell in modelling_scenario.pv_module.pv_cells
                     ]
                 ),
                 VOLTAGE_RESOLUTION,
@@ -2343,7 +2354,7 @@ def main(unparsed_arguments) -> None:
 
             # Compute the total power produced
             cell_voltage_interpreters = {
-                pv_cell: lambda i: np.interp(
+                pv_cell: lambda i, pv_cell=pv_cell: np.interp(
                     i,
                     list(reversed(mpp_cell_to_current_map[pv_cell])),
                     list(reversed(mpp_cell_to_voltage_map[pv_cell])),
@@ -2353,25 +2364,38 @@ def main(unparsed_arguments) -> None:
                 for pv_cell in modelling_scenario.pv_module.pv_cells_and_cell_strings
             }
 
+            sampling_current_series = np.array(
+                [
+                    entry
+                    for entry in sorted(current_series[::CURRENT_SAMPLING_RATE])
+                    if entry <= 10
+                ]
+            )
+
             cellwise_voltage = {
                 pv_cell: [
-                    cell_voltage_interpreters[pv_cell](current)
+                    interpreter(current)
                     for current in tqdm(
-                        current_series[::CURRENT_SAMPLING_RATE],
-                        desc="Interpolating current",
+                        sampling_current_series,
+                        desc="Interpolation calculation",
                         leave=False,
                     )
                 ]
-                for pv_cell in tqdm(
-                    modelling_scenario.pv_module.pv_cells_and_cell_strings,
-                    desc="String-wise calculation",
+                for pv_cell, interpreter in tqdm(
+                    cell_voltage_interpreters.items(),
+                    desc="Cell-wise calculation",
                     leave=False,
                 )
             }
+
             module_voltage = [
                 sum(sublist) for sublist in zip(*cellwise_voltage.values())
             ]
 
+            module_power = module_voltage * sampling_current_series
+            mpp_index: int = list(module_power).index(np.max(module_power))
+
+            plt.figure(figsize=(48 / 5, 32 / 5))
             for index, pv_cell in enumerate(
                 modelling_scenario.pv_module.pv_cells_and_cell_strings
             ):
@@ -2382,10 +2406,9 @@ def main(unparsed_arguments) -> None:
                     color=f"C{index}",
                 )
 
-            plt.plot(
-                module_voltage,
-                current_series[::CURRENT_SAMPLING_RATE],
-                "--",
+            plt.axhline(
+                sampling_current_series[mpp_index],
+                dashes=(2, 2),
                 color="orange",
             )
 
@@ -2394,6 +2417,30 @@ def main(unparsed_arguments) -> None:
             plt.xlabel("Cell-wise, or cell-string-wise, voltage / V")
             plt.ylabel("Module current / A")
 
+            plt.xlim(-1.25, 1.5)
+
+            norm = plt.Normalize(
+                0.5, len(modelling_scenario.pv_module.pv_cells_and_cell_strings) + 0.5
+            )
+            scalar_mappable = plt.cm.ScalarMappable(
+                cmap=mcolors.LinearSegmentedColormap.from_list(
+                    "Custom",
+                    sns.color_palette().as_hex(),
+                    len(set(modelling_scenario.pv_module.pv_cells_and_cell_strings))
+                    + 1,
+                ),
+                norm=norm,
+            )
+
+            colorbar = (axis := plt.gca()).figure.colorbar(
+                scalar_mappable,
+                ax=axis,
+                label="Index of PV cell or bypassed string of PV cells",
+                pad=(_pad := 0.025),
+            )
+
+            plt.legend().remove()
+
             plt.savefig(
                 f"iv_curves_with_module_{time_string}.pdf",
                 format="pdf",
@@ -2401,9 +2448,6 @@ def main(unparsed_arguments) -> None:
                 pad_inches=0,
             )
             plt.show()
-
-            module_power = module_voltage * current_series[::CURRENT_SAMPLING_RATE]
-            mpp_index: int = list(module_power).index(np.max(module_power))
 
             plt.figure(figsize=(48 / 5, 32 / 5))
             left_axis = plt.gca()
@@ -2420,14 +2464,14 @@ def main(unparsed_arguments) -> None:
                 )
 
             right_axis.plot(
-                current_series[::CURRENT_SAMPLING_RATE],
+                sampling_current_series,
                 module_power,
                 "--",
                 color="orange",
                 label="Module power",
             )
             right_axis.scatter(
-                current_series[::CURRENT_SAMPLING_RATE][mpp_index],
+                sampling_current_series[mpp_index],
                 module_power[mpp_index],
                 marker="H",
                 color="orange",
@@ -2435,11 +2479,15 @@ def main(unparsed_arguments) -> None:
                 label="Maximum power point (MPP)",
             )
 
-            left_axis.set_ylim(bottom=-25, top=25)
+            left_axis.set_ylim(bottom=-5, top=5)
             right_axis.set_ylim(
                 bottom=-(power_limit := 1.1 * np.max(module_power)), top=power_limit
             )
-            plt.xlim(np.min(current_series), np.max(current_series))
+            left_axis.set_xlim(0, 5)
+            right_axis.set_xlim(0, 5)
+
+            left_axis.axhline(0, dashes=(2, 2), color="grey")
+            right_axis.axhline(0, dashes=(2, 2), color="grey")
 
             l_handles, l_labels = left_axis.get_legend_handles_labels()
             r_handles, r_labels = right_axis.get_legend_handles_labels()
@@ -2450,9 +2498,34 @@ def main(unparsed_arguments) -> None:
                 #  title="Initial index of cell in string",
                 ncol=4,
             )
-            plt.xlabel("Module current / A")
+            left_axis.set_xlabel("Module current / A")
             left_axis.set_ylabel("Cell-wise, or cell-string-wise, power / W")
             right_axis.set_ylabel("Module power / W")
+
+            norm = plt.Normalize(
+                0.5, len(modelling_scenario.pv_module.pv_cells_and_cell_strings) + 0.5
+            )
+            scalar_mappable = plt.cm.ScalarMappable(
+                cmap=mcolors.LinearSegmentedColormap.from_list(
+                    "Custom",
+                    sns.color_palette().as_hex(),
+                    len(set(modelling_scenario.pv_module.pv_cells_and_cell_strings))
+                    + 1,
+                ),
+                norm=norm,
+            )
+
+            colorbar = (axis := plt.gca()).figure.colorbar(
+                scalar_mappable,
+                ax=axis,
+                label="Index of PV cell or bypassed string of PV cells",
+                pad=(_pad := 0.125),
+            )
+
+            plt.legend().remove()
+
+            left_axis.legend().remove()
+            right_axis.legend().remove()
 
             plt.savefig(
                 f"ip_curves_{time_string}.pdf",